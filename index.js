8beeb8376fb0167db0207e8070822867656701ae
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-08-12 09:10:01 -0400

    Reinitializing a clean repo

---
+++ index.js
@@ -0,0 +1,8 @@
+var _ = require('lodash');
+
+module.exports = _.extend(require('gulp-util'),{
+  ascii: require('./lib/ascii'),
+  Scaffold: require('./lib/Scaffold'),
+  ScaffoldStep: require('./lib/ScaffoldStep'),
+  inquirer: require('./lib/inquirer')
+});
---
+++ test/Scaffold.js
@@ -0,0 +1,10 @@
+var util = require('..');
+require('should');
+require('mocha');
+
+describe('Scaffold', function(){
+  it('should work on a null', function(done){
+    util.Scaffold({}).should.equal(util.Scaffold({}));
+    done();
+  });
+});
