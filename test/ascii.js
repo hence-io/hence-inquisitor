e87c5072b96e246cd881c861374d4767661927c9
Sean Homer <sean.homer@therefore.ca>
2015-08-13 12:31:37 -0400

    - looking to add travis-ci & automated coveralls support

--- test/ascii.js
+++ test/ascii.js
@@ -7,19 +7,19 @@ require('mocha');
 describe('ascii', function () {
   it('should format a heading', function (done) {
     ascii.heading('Test').should
-      .equal('\n\n ' + ascii.rawCodes.bold.start + 'Test' + ascii.rawCodes.bold.end + '\n\n');
+      .equal('\n\n ' + ascii.rawCodes.boldUnderline.start + 'Test' + ascii.rawCodes.boldUnderline.end + '\n\n');
     done();
   });
 
   it('should format a heading and output to console', function (done) {
     ascii.heading('Test', true).should
-      .equal('\n\n ' + ascii.rawCodes.bold.start + 'Test' + ascii.rawCodes.bold.end + '\n\n');
+      .equal('\n\n ' + ascii.rawCodes.boldUnderline.start + 'Test' + ascii.rawCodes.boldUnderline.end + '\n\n');
     done();
   });
 
   it('should render ascii art', function (done) {
     ascii.renderAsciiArt('----', 'Test').should
-      .equal('\n' + ascii.rawCodes.boldUnderline.start + '----' + ascii.rawCodes.boldUnderline.end + '\n' + 'Test' + '\n');
+      .equal('\n' + ascii.rawCodes.bold.start + '----' + ascii.rawCodes.bold.end + '\n' + 'Test' + '\n');
     done();
   });
 });
