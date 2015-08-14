13a747232b049e241ec9853a7c311da687ee2cde
Clint Beacock <clint@therefore.ca>
2015-08-14 13:08:55 -0400

    moved inquirer.validater to it's own export, renamed inquirer.validator function to inquirer.validatePrompt

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ module.exports = _.extend(require('gulp-util'),{
   ascii: require('./lib/ascii'),
   Scaffold: require('./lib/Scaffold'),
   ScaffoldStep: require('./lib/ScaffoldStep'),
-  inquirer: require('./lib/inquirer')
+  inquirer: require('./lib/inquirer'),
+  validator: require('./lib/validator')
 });
