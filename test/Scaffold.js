2001d00db3d1f5b6379cc9d8b3a401249448d45e
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-08-21 16:42:01 -0400

    - revising the usage of the multiInstall, keeping Scaffold more flexible, and focused on use this vs self for accessing the actual current instance

--- test/Scaffold.js
+++ test/Scaffold.js
@@ -5,7 +5,24 @@ require('mocha');
 describe('Scaffold', function(){
   it('should perform a blank install successfully', function(done){
     var step = util.ScaffoldStep({});
-    var scaffold = util.Scaffold({});
+    var scaffold = util.Scaffold({
+      //_debug: true,
+    });
     scaffold.start([step],done);
+  });
+  it('should perform a blank multi-install successfully', function(done){
+    var step = util.ScaffoldStep({});
+    var scaffold = util.Scaffold({
+      //_debug: true,
+      content: {
+        intro: 'Multi-install start',
+        done: "Multi-install complete."
+      }
+    });
+    scaffold.startMultiInstall([step],[
+      {option1:'x'},
+      {option1:'y'},
+      {option1:'z'}
+    ],done);
   });
 });
