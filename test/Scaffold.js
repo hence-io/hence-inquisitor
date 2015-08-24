42354e21506da49d852c59c74283cba2d3368e61
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-08-24 11:00:54 -0400

    - improving the scaffold test coverage

--- test/Scaffold.js
+++ test/Scaffold.js
@@ -1,16 +1,53 @@
-var util = require('..');
-require('should');
 require('mocha');
+var should = require('should');
+var util = require('..');
+var es = require('event-stream');
 
-describe('Scaffold', function(){
-  it('should perform a blank install successfully', function(done){
+describe('Scaffold', function () {
+  it('should perform a blank install successfully', function (done) {
     var step = util.ScaffoldStep({});
     var scaffold = util.Scaffold({
       //_debug: true,
     });
-    scaffold.start([step],done);
+    scaffold.start([step], function (err) {
+      should.not.exist(err);
+      done();
+    });
   });
-  it('should perform a blank multi-install successfully', function(done){
+
+  it('should perform a blank install and catch an error', function (done) {
+    var step = util.ScaffoldStep({});
+    var scaffold = util.Scaffold({
+      install: function (answers, finalize) {
+        should.exist(answers);
+
+        finalize('This should error');
+      }
+    });
+    scaffold.start([step], function (err) {
+      should.exist(err);
+      done();
+    });
+  });
+
+  it('should perform a blank install successfully with a fake install stream', function (done) {
+    var step = util.ScaffoldStep({});
+    var scaffold = util.Scaffold({
+      install: function (answers, finalize) {
+        var stream = es.readArray([1, 2, 3]);
+
+        should.exist(answers);
+
+        finalize(null, stream);
+      }
+    });
+    scaffold.start([step], function (err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
+  it('should perform a blank multi-install successfully', function (done) {
     var step = util.ScaffoldStep({});
     var scaffold = util.Scaffold({
       //_debug: true,
@@ -19,10 +56,10 @@ describe('Scaffold', function(){
         done: "Multi-install complete."
       }
     });
-    scaffold.startMultiInstall([step],[
-      {option1:'x'},
-      {option1:'y'},
-      {option1:'z'}
-    ],done);
+    scaffold.startMultiInstall([step], [
+      {option1: 'x'},
+      {option1: 'y'},
+      {option1: 'z'}
+    ], done);
   });
 });
